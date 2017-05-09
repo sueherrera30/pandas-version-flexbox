0acde0d7f6ff5e1dbd26ac7167731d0988861d7e
sue martinez <sueherrera30@gmail.com>
2017-05-09 16:37:53 +0000

    botones con funcionalidad

--- js/main.js
+++ js/main.js
@@ -19,3 +19,29 @@ for(var i=0;i < tachecitos.length; i++){
   tachecitos[i].parentNode.style.display="flex";
  }
 }
+// textos
+var origen = document.getElementsByClassName('botoncitos')[0];
+origen.addEventListener("click",origenMostrar);
+
+function origenMostrar(){
+ var texto1= document.getElementById("texto1");
+ if(texto1.style.visibility=="hidden"){
+   texto1.style.visibility="visible";
+ }
+ else{
+   texto1.style.visibility="hidden";
+ };
+}
+
+var extincion = document.getElementsByClassName('botoncitos')[1];
+extincion.addEventListener("click",extinguiendo);
+
+function extinguiendo(){
+  var texto2= document.getElementById("texto2");
+  if(texto2.style.visibility=="visible"){
+    texto2.style.visibility="hidden";
+  }
+  else{
+    texto2.style.visibility="visible";
+  };
+ }
